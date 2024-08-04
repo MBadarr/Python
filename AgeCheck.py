9c646668bf54a49d698dce14d2553d20c7ba9250
M.Badar <mbadar418@gmail.com>
2024-08-04 21:40:14 +0500

    Age Checker

--- AgeCheck.py
+++ AgeCheck.py
@@ -1,10 +1,6 @@
-age = input(int("Enter Your Age:"))
+age = int(input("Enter Your Age: "))
 
-if age >= "18":
+if age >= 18:
     print("You can login")
-elif age < "18":
-    print("You can't login")
-
 else:
-    print("Not Allowed")
-
+    print("You can't login")
