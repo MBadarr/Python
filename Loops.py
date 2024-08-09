18cfee6cbcdc2f6ed89238d710011ce356236d4a
M.Badar <mbadar418@gmail.com>
2024-08-09 22:32:52 +0500

    For Loop

--- Loops.py
+++ Loops.py
@@ -2,4 +2,10 @@ fruits = ["apple", "banana", "cherry"]
 for x in fruits:
     print(x)
 
+# Break Statement
 
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
